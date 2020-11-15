954c1b080800b0994f20da6476f21a52d34e18d0
TamtamHero <10632523+TamtamHero@users.noreply.github.com>
2020-11-15 18:12:26 +0100

    Move some dependancies to devDependancies + apply prettier

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React, { useState } from "react";
-import logo from './logo.svg';
-import RestoreButton from './components/RestoreButton';
-import BackupButton from './components/BackupButton';
+import logo from "./logo.svg";
+import RestoreButton from "./components/RestoreButton";
+import BackupButton from "./components/BackupButton";
 import PasswordsManager from "./controller/PasswordsManager.js";
 import AppExplanations from "./components/AppExplanations";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { listen } from "@ledgerhq/logs";
-import packageJson from '../package.json';
-import './App.css';
+import packageJson from "../package.json";
+import "./App.css";
 
 const passwordsManager = new PasswordsManager();
-listen(log => { console.log(log) });
+listen((log) => {
+  console.log(log);
+});
 
 function App() {
-
   function ask_device(device_handler, request) {
     return new Promise(async (resolve) => {
       let result = null;
@@ -32,14 +33,12 @@ function App() {
           toast.dismiss();
           toast.success("Success 🦄");
         }
-      }
-      catch (error) {
-        device_handler.disconnect()
+      } catch (error) {
+        device_handler.disconnect();
         setConnected(false);
         toast.dismiss();
         toast.error(`${error.toString()} 🙅`);
-      }
-      finally {
+      } finally {
         setBusy(false);
         resolve(result);
       }
@@ -60,13 +59,45 @@ function App() {
       </div>
       <header className="App-header">
         <div className="Commands">
-          <BackupButton text="Backup" color="#41ccb4" disabled={isBusy} hidden={!isConnected} onClick={() => ask_device(passwordsManager, () => passwordsManager.dump_metadatas())} />
-          <RestoreButton text="Restore" color="#FFB86D" disabled={isBusy} hidden={!isConnected} onClick={(metadatas) => ask_device(passwordsManager, () => passwordsManager.load_metadatas(metadatas))} />
-          <BackupButton text="Connect" color="#41ccb4" disabled={isBusy} hidden={isConnected} onClick={() => ask_device(passwordsManager)} />
+          <BackupButton
+            text="Connect"
+            color="#41ccb4"
+            disabled={isBusy}
+            hidden={isConnected}
+            onClick={() => ask_device(passwordsManager)}
+          />
+          <BackupButton
+            text="Backup"
+            color="#41ccb4"
+            disabled={isBusy}
+            hidden={!isConnected}
+            onClick={() =>
+              ask_device(passwordsManager, () =>
+                passwordsManager.dump_metadatas()
+              )
+            }
+          />
+          <RestoreButton
+            text="Restore"
+            color="#FFB86D"
+            disabled={isBusy}
+            hidden={!isConnected}
+            onClick={(metadatas) =>
+              ask_device(passwordsManager, () =>
+                passwordsManager.load_metadatas(metadatas)
+              )
+            }
+          />
         </div>
         <AppExplanations></AppExplanations>
         <div className="App-footer">
-          <p>A modest Web App built at Ledger with React, hosted by Github. v{`${packageJson.version}`}. <a href="https://github.com/LedgerHQ/passwords-backup">PRs welcomed and appreciated ✨</a></p>
+          <p>
+            A modest Web App built at Ledger with React, hosted by Github. v
+            {`${packageJson.version}`}.{" "}
+            <a href="https://github.com/LedgerHQ/passwords-backup">
+              PRs welcomed and appreciated ✨
+            </a>
+          </p>
         </div>
       </header>
     </div>
--- src/components/RestoreButton/index.js
+++ src/components/RestoreButton/index.js
@@ -1,45 +1,52 @@
 import React, { useRef, useCallback, useState } from "react";
-import './index.css';
+import "./index.css";
 
 function RestoreButton({ text, color, disabled, hidden, onClick }) {
-    const [isLoading, setLoading] = useState(false);
-    const file = useRef(null);
+  const [isLoading, setLoading] = useState(false);
+  const file = useRef(null);
 
-    const hasFileInputBeenCanceled = () => {
-        if (!file.current.value.length) setLoading(false);
-        document.body.onfocus = null;
-    }
+  const hasFileInputBeenCanceled = () => {
+    if (!file.current.value.length) setLoading(false);
+    document.body.onfocus = null;
+  };
 
-    const onTriggerFileSelect = useCallback(() => {
-        setLoading(true);
-        document.body.onfocus = hasFileInputBeenCanceled;
-        file.current && file.current.click();
-    }, []);
+  const onTriggerFileSelect = useCallback(() => {
+    setLoading(true);
+    document.body.onfocus = hasFileInputBeenCanceled;
+    file.current && file.current.click();
+  }, []);
 
-    const onSelectedFileChanged = useCallback((event) => {
-        event.target.files[0].text().then((text) => {
-            event.target.value = '';
-            onClick(text).then(() => setLoading(false));
-        });
-    }, [onClick]);
+  const onSelectedFileChanged = useCallback(
+    (event) => {
+      event.target.files[0].text().then((text) => {
+        event.target.value = "";
+        onClick(text).then(() => setLoading(false));
+      });
+    },
+    [onClick]
+  );
 
-    return (
-        <button
-            className="RestoreButton"
-            disabled={isLoading | disabled}
-            onClick={isLoading ? null : onTriggerFileSelect}
-            style={{ "margin": "10px", "backgroundColor": color, "display": hidden ? "none" : true }}
-        >
-            {isLoading ? 'Loading…' : text}
-            <input
-                type="file"
-                ref={file}
-                onChange={onSelectedFileChanged}
-                accept=".json"
-                style={{ "display": "none" }}
-            />
-        </button>
-    );
+  return (
+    <button
+      className="RestoreButton"
+      disabled={isLoading | disabled}
+      onClick={isLoading ? null : onTriggerFileSelect}
+      style={{
+        margin: "10px",
+        backgroundColor: color,
+        display: hidden ? "none" : true,
+      }}
+    >
+      {isLoading ? "Loading…" : text}
+      <input
+        type="file"
+        ref={file}
+        onChange={onSelectedFileChanged}
+        accept=".json"
+        style={{ display: "none" }}
+      />
+    </button>
+  );
 }
 
-export default RestoreButton
+export default RestoreButton;
